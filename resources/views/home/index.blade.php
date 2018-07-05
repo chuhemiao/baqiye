44823e78dc7a5161556dd5bd495f38ca80f1e19e
chuhemiao <535101602@qq.com>
2018-07-06 01:22:03 +0800

    kk

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.home')
+@extends('resources.views.layouts.home')
 @section('info')
     <title>{{Config::get('web.web_title')}} - {{Config::get('web.seo_title')}}</title>
     <meta name="keywords" content="{{Config::get('web.keywords')}}" />
@@ -14,22 +14,22 @@
             <div data-am-widget="slider" class="am-slider am-slider-c3" data-am-slider='{"controlNav":false}' >
                 <ul class="am-slides">
                     <li>
-                    <a href="/a/8"><img src="{{asset('resources/views/home/style/images/bing-1.jpg')}}">
+                    <a href="/a/8"><img src="{{asset('home/style/images/bing-1.jpg')}}">
                     <div class="am-slider-desc"><div class="am-slider-counter"><span class="am-active">1</span>/4</div>远方 有一个地方 那里种有我们的梦想</div></a>
 
                     </li>
                     <li>
-                    <a href="/a/8"><img src="{{asset('resources/views/home/style/images/bing-2.jpg')}}">
+                    <a href="/a/8"><img src="{{asset('home/style/images/bing-2.jpg')}}">
                     <div class="am-slider-desc"><div class="am-slider-counter"><span class="am-active">2</span>/4</div>某天 也许会相遇 相遇在这个好地方</div></a>
 
                     </li>
                     <li>
-                    <a href="/a/8"><img src="{{asset('resources/views/home/style/images/bing-3.jpg')}}">
+                    <a href="/a/8"><img src="{{asset('home/style/images/bing-3.jpg')}}">
                     <div class="am-slider-desc"><div class="am-slider-counter"><span class="am-active">3</span>/4</div>不要太担心 只因为我相信 终会走过这条遥远的道路</div></a>
 
                     </li>
                     <li>
-                    <a href="/a/8"><img src="{{asset('resources/views/home/style/images/bing-4.jpg')}}">
+                    <a href="/a/8"><img src="{{asset('home/style/images/bing-4.jpg')}}">
                     <div class="am-slider-desc"><div class="am-slider-counter"><span class="am-active">4</span>/4</div>OH PARA PARADISE 是否那么重要 你是否那么地遥远</div></a>
 
                     </li>
@@ -66,7 +66,7 @@
             <section class="am-panel am-panel-primary">
               <div class="am-panel-hd">关于我们</div>
               <div class="am-panel-bd">
-                <p class="am-thumbnail"><img src="{{asset('resources/views/home/style/images/baqiye_erweima.jpg')}}"  alt="扒企业"></p>
+                <p class="am-thumbnail"><img src="{{asset('home/style/images/baqiye_erweima.jpg')}}"  alt="扒企业"></p>
               </div>
             </section>
 
